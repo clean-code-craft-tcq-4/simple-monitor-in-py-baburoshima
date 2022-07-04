d98218b45e25678924c2413d1d16fb1e2c101beb
baburoshima <105203651+baburoshima@users.noreply.github.com>
2022-07-04 22:52:14 +0530

    adding boundary value testcases

--- check_limits.py
+++ check_limits.py
@@ -3,19 +3,19 @@ import test_check_limits as test
 
 def battery_is_ok(parameters):
   parameters = parameters + limits.Parameter_list
-  return batter_params_check_minlimit(parameters) and batter_params_check_maxlimit(parameters)
+  return battery_params_check_minlimit(parameters) and battery_params_check_maxlimit(parameters)
 
-def batter_params_check_minlimit(parameters):
+def battery_params_check_minlimit(parameters):
   for item in range(limits.Parameter_count):
-    if parameters[item] < limits.min_values[item]:
+    if parameters[item] <= limits.min_values[item]:
       print("FAIL :"+ parameters[item+limits.Parameter_count] + " less than min range!" )
       return False
   print("All parameters above min range")
   return True
 
-def batter_params_check_maxlimit(parameters):
+def battery_params_check_maxlimit(parameters):
   for item in range(limits.Parameter_count):
-    if parameters[item] > limits.max_values[item]:
+    if parameters[item] >= limits.max_values[item]:
       print("FAIL :"+ parameters[item+limits.Parameter_count] + " Greater than Max range!")
       return False 
   print("All parameters with max range")
--- test_check_limits.py
+++ test_check_limits.py
@@ -1,32 +1,41 @@
 import check_limits
 
 def check_limits_test():
+  #initial test cases provided
     assert(check_limits.battery_is_ok((25, 70, 0.7)) is True)
     print("-------------------------------")
     assert(check_limits.battery_is_ok((50, 85, 0)) is False)
     print("-------------------------------")
   #temparature limit tests
-    assert(check_limits.battery_is_ok((0, 70, 0.7)) is True)
+    assert(check_limits.battery_is_ok((0, 70, 0.7)) is False) # MIN limit
     print("-------------------------------")
-    assert(check_limits.battery_is_ok((45, 70, 0.7)) is True)
+    assert(check_limits.battery_is_ok((45, 70, 0.7)) is False) # MAX limit
     print("-------------------------------")
-    assert(check_limits.battery_is_ok((46, 70, 0.7)) is False)
+    assert(check_limits.battery_is_ok((46, 70, 0.7)) is False) #above MAX limit
     print("-------------------------------")
-    assert(check_limits.battery_is_ok((-1, 70, 0.7)) is False)
+    assert(check_limits.battery_is_ok((-1, 70, 0.7)) is False) # below MIN limit
+    print("-------------------------------")
+    assert(check_limits.battery_is_ok((30, 70, 0.7)) is True) #within limits
     print("-------------------------------")
   #soc limit tests
-    assert(check_limits.battery_is_ok((40, 20, 0.5)) is True)
+    assert(check_limits.battery_is_ok((40, 20, 0.5)) is False) # MIN limit
     print("-------------------------------")
-    assert(check_limits.battery_is_ok((40, 80, 0.5)) is True)
+    assert(check_limits.battery_is_ok((40, 80, 0.5)) is False) # MAX limit
     print("-------------------------------")
-    assert(check_limits.battery_is_ok((40, 19, 0.5)) is False)
+    assert(check_limits.battery_is_ok((40, 19, 0.5)) is False) # below MIN limit
     print("-------------------------------")
-    assert(check_limits.battery_is_ok((40, 81, 0.5)) is False)
+    assert(check_limits.battery_is_ok((40, 81, 0.5)) is False) # above MAX limit
+    print("-------------------------------")
+    assert(check_limits.battery_is_ok((40, 70, 0.5)) is True) # within limits
     print("-------------------------------")
   #charge rate tests
-    assert(check_limits.battery_is_ok((40, 25, 0.7)) is True)
+    assert(check_limits.battery_is_ok((40, 25, 0.1)) is False) # MIN limit
     print("-------------------------------")
-    assert(check_limits.battery_is_ok((40, 25, 0.8)) is True)
+    assert(check_limits.battery_is_ok((40, 25, 0.8)) is False) # MAX limit
     print("-------------------------------")
-    assert(check_limits.battery_is_ok((40, 25, 0.9)) is False)
+    assert(check_limits.battery_is_ok((40, 25, 0.9)) is False) # above MAX limit
+    print("-------------------------------")
+    assert(check_limits.battery_is_ok((40, 25, 0)) is False) # below MIN limit
+    print("-------------------------------")
+    assert(check_limits.battery_is_ok((40, 25, 0.25)) is True) # within limits
     print("-------------------------------")
