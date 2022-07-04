540e4bd5aee3dc8bd1f89766335e7759406cbba0
baburoshima <105203651+baburoshima@users.noreply.github.com>
2022-07-04 22:39:41 +0530

    removing duplicate prints

--- check_limits.py
+++ check_limits.py
@@ -3,22 +3,22 @@ import test_check_limits as test
 
 def battery_is_ok(parameters):
   parameters = parameters + limits.Parameter_list
-  batter_params_check_minlimit(parameters)
-  batter_params_check_maxlimit(parameters)
-  return batter_params_check_maxlimit(parameters) and batter_params_check_minlimit(parameters)
+  return batter_params_check_minlimit(parameters) and batter_params_check_maxlimit(parameters)
 
 def batter_params_check_minlimit(parameters):
   for item in range(limits.Parameter_count):
     if parameters[item] < limits.min_values[item]:
-      print(parameters[item+limits.Parameter_count] + " less than min range!" )
+      print("FAIL :"+ parameters[item+limits.Parameter_count] + " less than min range!" )
       return False
+  print("All parameters above min range")
   return True
 
 def batter_params_check_maxlimit(parameters):
   for item in range(limits.Parameter_count):
     if parameters[item] > limits.max_values[item]:
-      print(parameters[item+limits.Parameter_count] + " Greater than Max range!")
+      print("FAIL :"+ parameters[item+limits.Parameter_count] + " Greater than Max range!")
       return False 
+  print("All parameters with max range")
   return True
 
 if __name__ == '__main__':
