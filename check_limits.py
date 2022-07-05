19e98d00e50f28a02da0eca7508df4be1023d7c4
baburoshima <105203651+baburoshima@users.noreply.github.com>
2022-07-05 10:48:43 +0530

    Renamed functions

--- check_limits.py
+++ check_limits.py
@@ -2,10 +2,10 @@ import limits_definition as limits
 import test_check_limits as test
 
 def battery_is_ok(parameters):
-  parameters = parameters + limits.Parameter_list
-  return battery_params_check_minlimit(parameters) and battery_params_check_maxlimit(parameters)
+  parameters = parameters + limits.Parameter_name #append name of parameters
+  return Is_batteryparams_minlimit_ok(parameters) and Is_batteryparams_maxlimit_ok(parameters)
 
-def battery_params_check_minlimit(parameters):
+def Is_batteryparams_minlimit_ok(parameters):
   for item in range(limits.Parameter_count):
     if parameters[item] <= limits.min_values[item]:
       print("FAIL :"+ parameters[item+limits.Parameter_count] + " less than min range!" )
@@ -13,7 +13,7 @@ def battery_params_check_minlimit(parameters):
   print("All parameters above min range")
   return True
 
-def battery_params_check_maxlimit(parameters):
+def Is_batteryparams_maxlimit_ok(parameters):
   for item in range(limits.Parameter_count):
     if parameters[item] >= limits.max_values[item]:
       print("FAIL :"+ parameters[item+limits.Parameter_count] + " Greater than Max range!")
